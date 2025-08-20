8492e917eb0f1a209a76a9320128cb7db42c85dc
Dmitry Ponyatov <dponyatov@gmail.com>
2025-08-20 17:33:34 +0400

    Signed-off-by: Dmitry Ponyatov <dponyatov@gmail.com>

--- src/bpf.rs
+++ src/bpf.rs
@@ -1,35 +1,28 @@
-//! eBPF backend
+//! eBPF backend using aya library
 
 #![no_std]
 #![no_main]
 
-// Required export for some BPF loaders
-#[no_mangle]
-pub extern "C" fn _license() -> *const u8 {
-    b"GPL\0".as_ptr()
+use aya_ebpf::{
+    bindings::{XDP_DROP, XDP_PASS},
+    macros::xdp,
+    programs::XdpContext,
+};
+
+#[xdp]
+pub fn xdp_program(ctx: XdpContext) -> u32 {
+    match process_packet(&ctx) {
+        Ok(action) => action,
+        Err(_) => XDP_DROP,
+    }
+}
+
+fn process_packet(_ctx: &XdpContext) -> Result<u32, ()> {
+    // Packet processing logic here
+    Ok(XDP_PASS)
 }
 
 #[panic_handler]
-fn panic(_info: &PanicInfo) -> ! {
-    loop {}
-}
-
-#[alloc_error_handler]
-fn alloc_error(_layout: core::alloc::Layout) -> ! {
-    loop {}
-}
-
-// BPF program entry point (XDP example)
-#[no_mangle]
-pub extern "C" fn xdp_program(ctx: *mut u8) -> i32 {
-    // Your XDP program logic here
-    // Return XDP_PASS, XDP_DROP, etc.
-    2 // XDP_PASS
-}
-
-// Alternative: Traffic control program
-#[no_mangle]
-pub extern "C" fn tc_program(ctx: *mut u8) -> i32 {
-    // TC program logic
-    0 // TC_ACT_OK
+fn panic(_info: &core::panic::PanicInfo) -> ! {
+    unsafe { core::hint::unreachable_unchecked() }
 }

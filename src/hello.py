e1b402d8a1f495f1ffafd7df7a8a834a15d5c899
Dmitry Ponyatov <dponyatov@gmail.com>
2025-08-20 17:58:03 +0400

    Signed-off-by: Dmitry Ponyatov <dponyatov@gmail.com>

--- src/hello.py
+++ src/hello.py
@@ -1,3 +1,25 @@
-# https://habr.com/ru/articles/683566/
+#!/usr/bin/env python3
+
+## https://habr.com/ru/articles/683566/
 
 from bcc import BPF
+
+BPF_PROGRAM = r"""
+int hello(void *ctx) {
+  bpf_trace_printk("Hello world! clone() is calling\n");
+  return 0;
+}
+"""
+
+b = BPF(text=BPF_PROGRAM)
+clone = b.get_syscall_fnname("clone")
+b.attach_kprobe(event=clone, fn_name="hello")
+
+while True:
+    try:
+        (task, pid, cpu, flags, ts, msg) = b.trace_fields()
+    except ValueError:
+        continue
+    except KeyboardInterrupt:
+        break
+    print(f"{task.decode()} {msg.decode()}")
